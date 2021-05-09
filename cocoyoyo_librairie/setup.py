08ad1ca37df55ba308961eca938075b8dc98a97d
CAMARM_Flipz <armand.camponovo@icloud.com>
2021-05-09 09:24:01 +0200

    finish all file for pypi

--- cocoyoyo_librairie/setup.py
+++ cocoyoyo_librairie/setup.py
@@ -18,7 +18,7 @@ setup(
                       ],
     keywords='conversion',
     classifiers=[
-        "Development Status :: 1 - Version",
+        "Development Status :: 1 - Alpha",
         "Intended Audience :: Developers",
         'License :: OSI Approved :: MIT License',
         "Programming Language :: Python :: 3",
